2a4fed65cc2abf8c30a546d7f653a43c9ba8fc51
Kanishka2327 <vermakanishka410@gmail.com>
2025-01-04 11:36:00 +0530

    Added solution for 3 sum problem

---
+++ Linear/15. 3 sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans; //Triplet store
+        sort(nums.begin(), nums.end()); //sort whole array
+
+        for(int i =0; i < n;i++){
+            if(i>0 && nums[i] == nums[i-1]) continue; //skip duplicate i 
+            int j =i+1, k =n-1; //defining j(leftside) and k(rightmost) 
+            while(j<k){ 
+                int sum = nums[i] +nums[j]+ nums[k];
+                if(sum <0){
+                    j++;
+                }else if(sum > 0){
+                    k--;
+                }else{
+                    ans.push_back({nums[i], nums[j], nums[k]}); //store triplet in ans vector
+                    j++; k--;
+                    while(j < k && nums[j] == nums[j-1]) j++; // inc j when j occurs in duplicate
+                }
+            }
+        }
+        return ans;
+    }
+};
