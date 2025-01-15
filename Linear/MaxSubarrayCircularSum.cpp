1d85a4d72d745698e5b0036873116ae0efe50ce4
Kanishka2327 <vermakanishka410@gmail.com>
2025-01-15 22:03:01 +0530

    Added solution for maximum sum of circular subarray

---
+++ Linear/MaxSubarrayCircularSum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& arr) {
+        int totalsum =0; 
+        int currmin=0, currmax=0;
+        int maxsum= arr[0], minsum =arr[0];
+//finding maxsum and minsum , subtract minsum from totalsum to get the circular array sum
+        for(int i=0; i<arr.size();i++){
+            currmax = max(currmax+arr[i] , arr[i]); 
+            maxsum = max(maxsum , currmax); //finding max sum from array
+
+            currmin = min(currmin+arr[i], arr[i]);
+            minsum = min(currmin, minsum); // finding min sum from array
+
+            totalsum += arr[i];//computing totalsum with each iteration
+        } 
+        
+        int circularsum = totalsum - minsum;
+
+        if(minsum ==  totalsum){ //edge case where all elements are neg return the maximum sum
+            return maxsum;
+        }
+        return max(maxsum , circularsum);
+    }
+};
