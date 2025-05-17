c6c9bc32939689aade25be04561f40bc22d88044
Kanishka <116458403+Kanishka2327@users.noreply.github.com>
2025-05-17 23:26:54 +0530

    Update Number of Provinces bfs.cpp

--- Nonlinear/Number of Provinces bfs.cpp
+++ Nonlinear/Number of Provinces bfs.cpp
@@ -9,7 +9,6 @@ void bfs( vector<vector<int>>& isConnected, int u ,vector<bool> &visited ){
     while(!que.empty()){
         int curr = que.front();
         que.pop();
-
         for(int v =0 ; v< n ;v++){
             if(!visited[v] && isConnected[u][v] == 1){
                 bfs( isConnected, v , visited);
